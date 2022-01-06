417dc471578be397484848ecf82bfbef1825e99b
Aleksey <bellatorjs1@gmail.com>
2022-01-07 00:24:39 +0300

    Edit index.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
-function fn(    a,   b) {
-  const c = a + b; return c;
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
+function fn(a, b) {
+  const c = a + b;
+  return c;
 }
