f25127506cc8ab7eac05bed9886c7dad2e12f5ad
Aleksey <bellatorjs1@gmail.com>
2022-01-07 00:20:35 +0300

    error

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import _ from "lodash";
-
 console.log("Hello, Hexlet!");
 console.log(_.last(["one", "two"]));
-function fn(a, b) {
-  const c = a + b;
-  return c;
+function fn(    a,   b) {
+  const c = a + b; return c;
 }
